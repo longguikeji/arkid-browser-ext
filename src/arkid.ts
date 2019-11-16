d0795f9e70b074ff74c4916881d10c4c8cdaac61
雷韩 <leihan@leihandeiMac.local>
2019-11-16 17:01:43 +0800

    fix:fix tabsize

--- src/arkid.ts
+++ src/arkid.ts
@@ -5,7 +5,6 @@ chrome.storage.sync.get(['arkUrl'], (result) => {
   if (result.arkUrl && result.arkUrl === originUrl) {
     let arkObserver = new MutationObserver(() => {
       token = localStorage.getItem('oneid')
-      console.log(token)
 
       if (token) {
         chrome.runtime.sendMessage({
@@ -15,10 +14,10 @@ chrome.storage.sync.get(['arkUrl'], (result) => {
     })
 
     arkObserver.observe(document.documentElement, {
-        attributes: true,
-        characterData: true,
-        childList: true,
-        subtree: true,
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true,
     })
   }
 })
--- src/background.ts
+++ src/background.ts
@@ -5,65 +5,65 @@ let oldUrl: string|null = null
 let arkUrl: string|null = null
 
 chrome.storage.sync.get(['arkUrl'], (result) => {
-    arkUrl = result.arkUrl
+  arkUrl = result.arkUrl
 })
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
-    if (request.token) { 
-        arkToken = request.token
-    }
+  if (request.token) { 
+    arkToken = request.token
+  }
 
-    if (arkToken && request.hasPasswordInput) {
+  if (arkToken && request.hasPasswordInput) {
 
-        Promise.resolve(getUserInfo(sender.url))
-            .then(
-                data => sendResponse(data)
-            )
-        return true
+    Promise.resolve(getUserInfo(sender.url))
+      .then(
+        data => sendResponse(data)
+      )
+    return true
 
-    } else {
-        sendResponse(null)
-    }
+  } else {
+    sendResponse(null)
+  }
 })
 
 async function getUserInfo(url: string) {
-    if (oldUrl === url) {
-        return
-    }
-    oldUrl = url
+  if (oldUrl === url) {
+    return
+  }
+  oldUrl = url
 
-    const urlArr = url.split('/')
-    if (urlArr[2]) {
-        url = urlArr[2]
-    }
+  const urlArr = url.split('/')
+  if (urlArr[2]) {
+    url = urlArr[2]
+  }
 
-    if (!url) {
-        return
-    }
+  if (!url) {
+    return
+  }
 
-    const userInfo = await userInfoApi({
-        domain: url,
-        token: arkToken,
-    })
+  const userInfo = await userInfoApi({
+    domain: url,
+    token: arkToken,
+  })
 
-    return userInfo
+  return userInfo
 }
 
 async function userInfoApi(q: {
-    domain: string,
-    token: string
+  domain: string,
+  token: string
 }) {
-    const url = `${arkUrl}/siteapi/oneid/ucenter/sub_account/`
+  const url = `${arkUrl}/siteapi/oneid/ucenter/sub_account/`
 
-    const data = await axios.get(url, {
-        params: {domain: q.domain},
-        headers: {'Authorization': 'token ' + q.token}
-    }).then((resp) => {
-        return resp.data.results
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    return data
+  const data = await axios.get(url, {
+    params: {domain: q.domain},
+    headers: {'Authorization': 'token ' + q.token}
+  }).then((resp) => {
+    return resp.data.results
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  return data
 }
